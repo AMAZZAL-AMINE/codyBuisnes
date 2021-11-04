669637e26457381783effd600ce5efc4ba53dd2b
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-11-04 19:05:32 +0100

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
 
     public function ShowProducts() {
 
-        $products = Product::orderBy('created_at','desc');
+        $products = Product::orderBy('created_at','desc')->get();
         $top_product = Product::where('is_top',"yes")->get();
         
         return view('products.products', compact('products', 'top_product'));
